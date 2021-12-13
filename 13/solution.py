9a17e07b7148d544aebabd998812a2592fd32e91
Xael <xael5813@gmail.com>
2021-12-13 16:46:14 +0700

    Day 13: Get rid of paper, use dots only

--- 13/solution.py
+++ 13/solution.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 input = open('input').read().splitlines()
 
 b = 0
@@ -7,8 +5,8 @@ M = 0
 m = 0
 N = 0
 n = 0
-dots = 0
-paper = defaultdict(bool)
+dot_count = 0
+dots = set() 
 for line in input:
   b += 1
   if not line:
@@ -16,21 +14,21 @@ for line in input:
   j, i = [int(x) for x in line.split(',')]
   M = max(i, M)
   N = max(j, N)
-  dots += 1
-  paper[(i, j)] = True
-  
+  dot_count += 1
+  dots.add((i, j))
+
 intructions = input[b:]
 
-def print_paper(m, M, n, N, dots):
+def print_paper(m, M, n, N, dot_count):
   print()
-  print("({}, {}), ({}, {}), {}".format(m, M, n, N, dots))
+  print("({}, {}), ({}, {}), {}".format(m, M, n, N, dot_count))
   for i in range(m, M+1):
     for j in range(n, N+1):
-      x = '#' if paper[(i, j)] else '.'
+      x = '#' if (i, j) in dots else ' '
       print(x, end='')
     print()
 
-def fold(dims, intruction, dots):
+def fold(dims, intruction, dot_count):
   axis, line = intruction.split(' ')[-1].split('=')
   line = int(line)
   m, M, n, N = dims
@@ -40,17 +38,12 @@ def fold(dims, intruction, dots):
   i = 1
   while line-i in range(m, M+1) and line+i in range(m, M+1):
     for j in range(n, N+1):
-      pos = ()
-      if axis == 'x':
-        pos = (j, line-(i*d))
-        op_pos = (j, line+(i*d))
-      else:
-        pos = (line-(i*d), j)
-        op_pos = (line+(i*d), j)
-
-      if paper[pos] & paper[op_pos]:
-        dots -= 1
-      paper[pos] |= paper[op_pos]
+      pos, inv_pos = ((j, line-i*d), (j, line+i*d)) if axis == 'x'  \
+          else ((line-i*d, j), (line+i*d, j))
+      if pos in dots and inv_pos in dots:
+        dot_count -= 1
+      elif pos in dots or inv_pos in dots:
+        dots.add(pos)
     i += 1
 
   if line < M//2:
@@ -58,9 +51,9 @@ def fold(dims, intruction, dots):
   else:
     M = line-1
   
-  return (m, M, n, N, dots) if axis == 'y' else (n, N, m, M, dots)
+  return (m, M, n, N, dot_count) if axis == 'y' else (n, N, m, M, dot_count)
 
 for intruction in intructions:
-  m, M, n, N, dots = fold((m, M, n, N), intruction, dots);
+  m, M, n, N, dot_count = fold((m, M, n, N), intruction, dot_count);
     
-print_paper(m, M, n, N, dots)
+print_paper(m, M, n, N, dot_count)
